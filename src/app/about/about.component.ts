1da9319331a5e3818a474840097c3b4af868266b
haddock <lchatela@hotmail.com>
2022-02-16 23:59:43 +0100

    Assignment 2 v1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Leader } from '../shared/leader'
+import { LeaderService } from '../services/leader.service';
+
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leaders: Leader[];
+  selectedLeader: Leader;
+
+  constructor(private leaderservice: LeaderService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+      this.leaders = this.leaderservice.getLeaders();
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
+
+
+
+
+
+
